4c86f4012daa9360c2c7b83d533ceb4ed7ccb387
Jason Barto <jason.p.barto@gmail.com>
2018-03-03 00:14:24 +0000

    added 2 helper scripts for running code in SageMaker and downloading model test data

---
+++ download_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SOURCE_URL='http://yann.lecun.com/exdb/mnist/'
+
+mkdir data
+
+function download_file () {
+    curl -o data/$1 ${SOURCE_URL}/$1
+    gunzip data/$1
+}
+
+download_file train-images-idx3-ubyte.gz
+download_file train-labels-idx1-ubyte.gz
+download_file t10k-images-idx3-ubyte.gz
+download_file t10k-labels-idx1-ubyte.gz
