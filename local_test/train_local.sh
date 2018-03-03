157c6220b07e2d01e20843531f01fa117714940a
Jason Barto <jason.p.barto@gmail.com>
2018-03-03 00:29:04 +0000

    added propogation from the data directory to the test_dir

--- local_test/train_local.sh
+++ local_test/train_local.sh
@@ -8,4 +8,10 @@ mkdir -p test_dir/output
 rm test_dir/model/*
 rm test_dir/output/*
 
+rm -fr test_dir/input/data/training/*
+rm -fr test_dir/input/data/eval/*
+
+cp ../data/train-* test_dir/input/data/training
+cp ../data/t10k-* test_dir/input/data/eval
+
 docker run -v $(pwd)/test_dir:/opt/ml --rm ${image} train
